4892a4a6829925c0fed8d9ba5cee93ccb1f0971e
tomigua <tomyguagnini@gmail.com>
2022-07-01 14:52:16 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} /> 
           <Route path="/movie/:id" element={<Movie/>} /> 
-
         </Routes>
       </Router>
     </>
